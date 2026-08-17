d6ec8172a13bc437d9509ba4e96d9a89ff700535
ZhouWei <1244620067@qq.com>
2026-08-17 13:29:44 +0800

    fix(dialog): pop up failure

--- apps/playground/src/demos/vapor/dialog/basic.vue
+++ apps/playground/src/demos/vapor/dialog/basic.vue
@@ -1,38 +1,26 @@
 <script setup vapor lang="ts">
   import Dialog from '@vapor-component/dialog'
-  import { computed, ref } from 'vue'
-
-  import { useDialog } from '@/composables/useDialog.ts'
+  import { useDialog } from '@/composables/useDialog'
   import '@/styles/dialog.less'
 
   const { visible } = useDialog()
-  const divRef = ref<HTMLDivElement | null>(null)
-  const getContainer = computed(() => () => divRef.value)
 </script>
 
 <template>
   <label>
     Dialog:
-    <button @click="visible = !visible">Open Dialog</button>
+    <button @click="visible = true">Open Dialog</button>
     <Dialog
       v-model:visible="visible"
       title="Dialog Title"
-      :get-container="getContainer"
-      :force-render="true"
       :mask-closable="false"
-      @close="
-        () => {
-          console.log('Dialog closed')
-          visible = !visible
-        }
-      "
+      @close="() => { console.log('Dialog closed'); visible = false }"
     >
       <p>This is the content of the dialog.</p>
       <p>You can put any content here, such as forms, text, or images.</p>
       <template #footer>
-        <button @click="visible = !visible">Close</button>
+        <button @click="visible = false">Close</button>
       </template>
     </Dialog>
-    <div ref="divRef" style="display: none" />
   </label>
 </template>
